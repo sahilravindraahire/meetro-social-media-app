4a66f6538db9ac7161e3218f6aec8a0ea2ae27a2
sahil ahire <sahilahire2003@gmail.com>
2026-06-07 21:03:28 +0530

    nodemailer change

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -6,27 +6,11 @@ import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
     service: "Gmail",
-    port: 2525,
-    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 })
-
-// export const sendEmail = async(to, otp) => {
-//     await transporter.sendMail({
-//         from:`MEETRO support: <${process.env.EMAIL_USER}>`,
-//         to,
-//         subject: "Reset Your Password",
-//         html: `
-//         <h2>Welcome to MEETRO!</h2>
-//         <p>Your OTP for password reset :</p>
-//         <h1 style="letter-spacing: 4px">${otp}</h1>
-//         <p>This OTP is valid for <strong>5 minutes</strong>.</p>
-//         `
-//     })
-// }
 
 
 export const sendEmail = async(to, otp) => {
