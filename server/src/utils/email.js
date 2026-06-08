a5e96e5c3dd92db495eeec422c5af1c994b2a620
sahil ahire <sahilahire2003@gmail.com>
2026-06-08 11:20:07 +0530

    xyz

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -1,38 +1,7 @@
-// import dotenv from "dotenv"
-// dotenv.config()
-
 import nodemailer from "nodemailer"
+import dotenv from "dotenv"
 
-
-// const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS
-//     }
-// })
-
-
-// export const sendEmail = async(to, otp) => {
-    
-//     try {
-//         await transporter.sendMail({
-//             from: `MEETRO support: <${process.env.EMAIL_USER}>`,
-//             to,
-//             subject: "Reset Your Password",
-//             html: `
-//             <h2>Welcome to MEETRO!</h2>
-//             <p>Your OTP for password reset :</p>
-//             <h1 style="letter-spacing: 4px">${otp}</h1>
-//             <p>This OTP is valid for <strong>5 minutes</strong>.</p>
-//             `
-//         })
-//         console.log("Email sent successfully")
-//     } catch (error) {
-//         console.log("Email sending failed:", error.message)
-//         throw error
-//     }
-// }
+dotenv.config()
 
 export const sendEmail = async (email, otp) => {
     try {
@@ -48,7 +17,7 @@ export const sendEmail = async (email, otp) => {
             from: `meetroo Support <${process.env.EMAIL_USER}>`,
             to: email,
             subject: `OTP for new password`,
-            text: `
+            html: `
             <h2>Welcome to MEETRO!</h2>
             <p>Your OTP for password reset :</p>
             <h1 style="letter-spacing: 4px">${otp}</h1>
