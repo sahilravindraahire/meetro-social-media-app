3ee5c8bd916bceac845e16d74fa84f043e1991eb
sahil ahire <sahilahire2003@gmail.com>
2026-06-08 11:31:10 +0530

    nga

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -3,15 +3,48 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
+// export const sendEmail = async (email, otp) => {
+//     try {
+//         const transporter = nodemailer.createTransport({
+//             service: "gmail",
+//             auth: {
+//                 user: process.env.EMAIL_USER,
+//                 pass: process.env.EMAIL_PASS
+//             }
+//         })
+
+//         const mailOptions = {
+//             from: `meetroo Support <${process.env.EMAIL_USER}>`,
+//             to: email,
+//             subject: `OTP for new password`,
+//             html: `
+//             <h2>Welcome to MEETRO!</h2>
+//             <p>Your OTP for password reset :</p>
+//             <h1 style="letter-spacing: 4px">${otp}</h1>
+//             <p>This OTP is valid for <strong>5 minutes</strong>.</p>
+//             `
+//         }
+
+//         await transporter.sendMail(mailOptions)
+//     } catch (error) {
+//         console.error(`Failed to send email to ${email}: ${error.message}`)
+//     }
+// }
+
 export const sendEmail = async (email, otp) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            service: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             }
         })
+
+        await transporter.verify()
+        console.log("smtp connected")
 
         const mailOptions = {
             from: `meetroo Support <${process.env.EMAIL_USER}>`,
